c6334eb70787c507d2720ab559ef919ff4de8a7d
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-06-19 21:19:48 +0200

    Add fontAwsome for empty component

--- src/Components/ListBook/Book/Empty/empty.jsx
+++ src/Components/ListBook/Book/Empty/empty.jsx
@@ -1,8 +1,11 @@
+import { faFaceSadCry } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
 export default function Empty() {
     return (
-        <div className="col-12">
+        <div className="d-flex flex-column align-items-center col-12">
+            <FontAwesomeIcon icon={faFaceSadCry} size="10x" />
             <h1 className="text-center">No results found</h1>
         </div>
     )
