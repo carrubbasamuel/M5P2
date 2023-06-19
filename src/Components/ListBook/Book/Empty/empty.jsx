73b36e911cefa434eb32e1e4728627ddb2358fec
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-06-19 21:11:14 +0200

    empty div add

---
+++ src/Components/ListBook/Book/Empty/empty.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Empty() {
+    return (
+        <div className="col-12">
+            <h1 className="text-center">No results found</h1>
+        </div>
+    )
+}
--- src/Components/ListBook/Book/book.jsx
+++ src/Components/ListBook/Book/book.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Empty from "./Empty/empty.jsx";
 import PaginationComponent from "./Pagination/pagination";
 import "./book.css";
 
@@ -28,7 +29,7 @@ export default function Book({ search, categoryArray, category }) {
   }, [category]);
 
 
-  
+
   useEffect(() => {
     if (search === "") {
       setFilter(categoryArray);
@@ -36,30 +37,36 @@ export default function Book({ search, categoryArray, category }) {
       let filterArray = categoryArray.filter((book) =>
         book.title.toLowerCase().includes(search.toLowerCase())
       );
-
       setFilter(filterArray);
     }
   }, [search, categoryArray]);
 
+
+
   return (
     <div>
       <div className="row justify-content-center align-items-start">
-        {filter
-          .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-          .map((book, index) => (
+        {filter.length > 0 ? filter.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((book, index) => (
             <div className="col-3" key={index}>
               <img src={book.img} alt="book cover" />
               <h1>{book.title}</h1>
             </div>
-          ))}
+          ))
+        : <Empty />
+        }
       </div>
 
       <div className="d-flex justify-content-center align-items-start mt-5">
-        <PaginationComponent
+        {
+          filter.length >0
+          && <PaginationComponent
           currentPage={currentPage}
           totalPages={totalPages}
           onPageChange={handlePageChange}
+          
         />
+        }
+        
       </div>
     </div>
   );
